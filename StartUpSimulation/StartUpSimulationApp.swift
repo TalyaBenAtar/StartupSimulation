97b31395adac6a0fa10513c0c95e91528aade400
TalyaBenAtar <talya159321@gmail.com>
2026-07-15 13:41:17 +0300

    Initial Commit

---
+++ StartUpSimulation/StartUpSimulationApp.swift
@@ -0,0 +1,17 @@
+//
+//  StartUpSimulationApp.swift
+//  StartUpSimulation
+//
+//  Created by Talya Benatar on 15/07/2026.
+//
+
+import SwiftUI
+
+@main
+struct StartUpSimulationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
